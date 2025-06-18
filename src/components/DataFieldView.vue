1d37502e8c8b8e5e20b9ccebeb6376f786e73dd2
mrudolph <michael.rudolph@open-dpp.de>
2025-06-18 11:05:09 +0200

    feat: Add ProductPassportLinkView and update field view logic
    Introduced `TextFieldView` and `ProductPassportLinkView` components. Updated `DataFieldView` to dynamically load components based on field type. Enhanced routing and adjusted layout for improved maintainability and usability.

--- src/components/DataFieldView.vue
+++ src/components/DataFieldView.vue
@@ -1,24 +1,23 @@
 <template>
   <div :class="[generateClassesForLayout(props.fieldView.layout)]">
     <div class="bg-white rounded-lg border border-gray-200 p-4 shadow-sm">
-      <dt class="mb-1 text-sm font-medium text-gray-500">
-        {{ props.fieldView.name }}
-      </dt>
-      <dd
-        :class="[
-          'text-sm font-medium text-gray-900',
-          generateClassesForLayout(props.fieldView.layout),
-        ]"
-      >
-        {{ props.fieldView.value }}
-      </dd>
+      <TextFieldView
+        v-if="props.fieldView.type === DataFieldType.TEXT_FIELD"
+        :field-view="props.fieldView"
+      />
+      <ProductPassportLinkView
+        v-if="props.fieldView.type === DataFieldType.PRODUCT_PASSPORT_LINK"
+        :field-view="props.fieldView"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { FieldViewDto } from "@open-dpp/api-client";
+import { DataFieldType, FieldViewDto } from "@open-dpp/api-client";
 import { generateClassesForLayout } from "../lib/layout.ts";
+import TextFieldView from "./TextFieldView.vue";
+import ProductPassportLinkView from "./ProductPassportLinkView.vue";
 
 const props = defineProps<{ fieldView: FieldViewDto }>();
 </script>
--- src/view/View.vue
+++ src/view/View.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="flex flex-col items-center gap-5 p-3">
-    <div v-if="indexStore.view !== 'client'" title="Certification" class="flex w-full">
+    <div
+      v-if="indexStore.view !== 'client'"
+      title="Certification"
+      class="flex w-full"
+    >
       <div class="w-full overflow-hidden bg-white shadow-sm sm:rounded-lg p-3">
-        <VerificationHeader/>
+        <VerificationHeader />
       </div>
     </div>
-    <ViewInformation/>
+    <ViewInformation />
   </div>
 </template>
 
 <script lang="ts" setup>
-import {useRoute} from "vue-router";
-import {onMounted} from "vue";
+import { useRoute } from "vue-router";
+import { watch } from "vue";
 import ViewInformation from "../components/ViewInformation.vue";
 import VerificationHeader from "../components/VerificationHeader.vue";
-import {useViewStore} from "../stores/product.ts";
+import { useViewStore } from "../stores/product.ts";
 import apiClient from "../lib/api-client.ts";
-import {useIndexStore} from "../stores";
+import { useIndexStore } from "../stores";
 
 const route = useRoute();
 
 const viewStore = useViewStore();
 const indexStore = useIndexStore();
 
-onMounted(async () => {
-  const response = await apiClient.uniqueProductIdentifiers.getView(String(route.params.permalink));
-  viewStore.view = response.data;
-});
+watch(
+  () => route.params.permalink,
+  async () => {
+    const response = await apiClient.uniqueProductIdentifiers.getView(
+      String(route.params.permalink),
+    );
+    viewStore.view = response.data;
+  },
+  { immediate: true },
+);
 </script>
