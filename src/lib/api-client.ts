3d3a9def5266d7f67c0143b64e354d7b83e9390e
mrudolph <michael.rudolph@open-dpp.de>
2025-07-11 17:51:30 +0200

    refactor: Adjust API client configuration to use `dpp` object for baseURL

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -2,7 +2,7 @@ import { OpenDppClient } from "@open-dpp/api-client";
 import { API_ROOT_URL } from "../const";
 
 const apiClient = new OpenDppClient({
-  baseURL: API_ROOT_URL,
+  dpp: { baseURL: API_ROOT_URL },
 });
 
 export default apiClient;
