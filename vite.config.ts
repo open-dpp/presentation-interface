e22b7dfecc911d919fb9c14d7dde7299ff611144
Florian Bieck <info@florianbieck.com>
2025-07-26 22:51:40 +0200

    Specify `radix` parameter in `parseInt` to ensure proper behavior and add fallback default port.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: parseInt(process.env.VITE_PORT || "5173"),
+    port: parseInt(process.env.VITE_PORT || "5173", 10) || 5173,
   },
 });
