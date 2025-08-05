e8efd22e3ec76143d0e4870ae0e9ee1ed575693f
mrudolph <michael.rudolph@open-dpp.de>
2025-08-05 09:32:37 +0200

    feat: Add Cypress test for rendering product passport sections and mock API responses
    Enhanced the "renders drafts and creates a new one" test in `View.cy.ts` to include mocked API responses for product passports. Introduced `cy.intercept` to simulate API calls and validate the visibility of dynamically rendered sections.

--- src/view/View.cy.ts
+++ src/view/View.cy.ts
@@ -1,6 +1,12 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 import { routes } from "../router";
 import View from "./View.vue";
+import {
+  DataFieldType,
+  GranularityLevel,
+  ProductPassportDto,
+  SectionType,
+} from "@open-dpp/api-client";
 
 const router = createRouter({
   history: createMemoryHistory(),
@@ -11,10 +17,43 @@ describe("<View />", () => {
   it("renders drafts and creates a new one", () => {
     const uuid = "uuid1";
 
-    cy.wrap(router.push(`/uuid`));
-    cy.mountWithPinia(View, { router });
+    const productPassport: ProductPassportDto = {
+      id: uuid,
+      name: "Product Name",
+      description: "Product Description",
+      dataSections: [
+        {
+          id: "s1",
+          name: "section 1",
+          type: SectionType.GROUP,
+          parentId: undefined,
+          subSections: [],
+          granularityLevel: GranularityLevel.MODEL,
+          dataFields: [
+            {
+              id: "f1",
+              name: "field 1",
+              type: DataFieldType.TEXT_FIELD,
+              granularityLevel: GranularityLevel.MODEL,
+            },
+          ],
+          dataValues: [
+            {
+              f1: "f1 value",
+            },
+          ],
+        },
+      ],
+    };
 
-    cy.contains("Verbindungen").should("be.visible");
-    cy.contains("Alle Ihre Verbindungen").should("be.visible");
+    cy.intercept("GET", `/product-passports/${uuid}`, {
+      statusCode: 200,
+      body: productPassport, // Mock response
+    }).as("getProductPassport");
+
+    cy.wrap(router.push(`/${uuid}`));
+    cy.mountWithPinia(View, { router });
+    cy.wait("@getProductPassport").its("response.statusCode").should("eq", 200);
+    cy.contains("section 1").should("be.visible");
   });
 });
