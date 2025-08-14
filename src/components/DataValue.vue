00bd651c0d3e5402ef1dd7fdacfb62feeacbc69a
Florian Bieck <info@florianbieck.com>
2025-08-14 15:46:07 +0200

    Add `DataFieldFileValue` component for file rendering and update Axios integration
    Introduced a new `DataFieldFileValue` component to display and download file-type fields. Updated `DataValue` to conditionally include this component for `FILE_FIELD` types. Added Axios instance with response interceptors and authorization header support. Refactored environment variable constants and integrated `setAxiosAuthHeader` with Keycloak authentication. Included `.env.example` for development configuration.

--- src/components/DataValue.vue
+++ src/components/DataValue.vue
@@ -5,6 +5,12 @@
     "
     :field-view="props.fieldView"
   />
+  <DataFieldFileValue
+      v-if="
+      props.fieldView.dataField.type === DataFieldType.FILE_FIELD
+    "
+      :field-view="props.fieldView"
+  />
   <span v-else>{{ props.fieldView.value }}</span>
 </template>
 
@@ -12,6 +18,7 @@
 import { FieldView } from "../lib/field-view";
 import { DataFieldType } from "@open-dpp/api-client";
 import LinkValue from "./LinkValue.vue";
+import DataFieldFileValue from "./DataFieldFileValue.vue";
 
 const props = defineProps<{ fieldView: FieldView }>();
 </script>
---
+++ src/lib/axios.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { logout } from "./keycloak";
+import { API_URL } from "../const";
+
+const axiosIns = axios.create({
+  baseURL: API_URL,
+});
+
+axiosIns.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    // const authStore = useAuthStore();
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      error.message === "Unauthorized"
+    ) {
+      logout();
+    }
+    return Promise.reject(error);
+  },
+);
+
+export const setAxiosAuthHeader = (token: string) => {
+  axiosIns.defaults.headers.common.Authorization = "Bearer " + token;
+};
+
+export default axiosIns;
